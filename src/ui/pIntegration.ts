057f67edf578d45c3926fe93dc4e5dcab9d4692a
Esther Sue <esthersu@amazon.com>
2023-06-27 15:04:19 -0700

    more syntax highlighting done

--- src/ui/pIntegration.ts
+++ src/ui/pIntegration.ts
@@ -5,6 +5,6 @@ import RelatedErrorView from "./relatedErrorView";
 
 export default function createAndRegisterPIntegration(installer: PInstaller): void {
     RelatedErrorView.createAndRegister(installer.context);
-    // GutterIcon.createAndRegister(installer.context);
+    GutterIcon.createAndRegister(installer.context);
     // CompileCommands.createAndRegister();
 }
