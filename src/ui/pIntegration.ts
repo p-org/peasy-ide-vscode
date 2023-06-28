832b2da25d92cc48df5e617fe3542c2c93f131bc
Esther Sue <esthersu@amazon.com>
2023-06-28 15:19:11 -0700

    Finished syntax highlighting and first version of testing framework done

--- src/ui/pIntegration.ts
+++ src/ui/pIntegration.ts
@@ -1,10 +1,9 @@
 import { PInstaller } from "../language/PInstallation";
-import CompileCommands from "./compileCommands";
-import GutterIcon from "./gutterIcon";
 import RelatedErrorView from "./relatedErrorView";
+import TestingEditor from "./testinginEditor";
 
 export default function createAndRegisterPIntegration(installer: PInstaller): void {
     RelatedErrorView.createAndRegister(installer.context);
-    GutterIcon.createAndRegister(installer.context);
+    TestingEditor.createAndRegister(installer.context);
     // CompileCommands.createAndRegister();
 }
