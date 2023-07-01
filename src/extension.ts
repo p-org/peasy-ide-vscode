034e585b2bb298bbecf54359ba30d5abd883309f
Esther Sue <esthersu@amazon.com>
2023-06-30 21:29:44 -0700

    changing names

--- src/extension.ts
+++ src/extension.ts
@@ -18,16 +18,8 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "p-compile-lsp" is !');
+	console.log('Congratulations, your extension "Ps Extension" is live!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('p-compile-lsp.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from p-compile-lsp!');
-	});
 
 	const statusOutput = window.createOutputChannel(ExtensionConstants.ChannelName);
 	context.subscriptions.push(statusOutput);
