4e22d5984c83d73f0deeb727d330a7a373fe2de6
Esther Sue <esthersu@amazon.com>
2023-07-10 13:50:29 -0700

    I fixed the compilation and testing errors and made it so users can run tests on both bash and zsh terminals

--- src/ui/pIntegration.ts
+++ src/ui/pIntegration.ts
@@ -1,9 +1,10 @@
 import { PInstaller } from "../language/PInstallation";
+import CompileCommands from "./compileCommands";
 import RelatedErrorView from "./relatedErrorView";
 import TestingEditor from "./testinginEditor";
 
 export default function createAndRegisterPIntegration(installer: PInstaller): void {
+    CompileCommands.createAndRegister();
     RelatedErrorView.createAndRegister(installer.context);
     TestingEditor.createAndRegister(installer.context);
-    // CompileCommands.createAndRegister();
 }
