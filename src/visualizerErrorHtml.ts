0dc50426bc712fff2eec9b47205344450e178cae
haoran-wen <hranwen@amazon.com>
2023-07-24 16:58:45 -0700

    fixed stylesUriMap indexing error

--- src/visualizerErrorHtml.ts
+++ src/visualizerErrorHtml.ts
@@ -8,9 +8,9 @@ const traceNotFoundHtml = (
     <html>
         <head>
             <meta charset="utf-8">
-            <link href="${stylesUriMap.shivizStylesUri}" type="text/css" rel="stylesheet">
-                  <link href="${stylesUriMap.stylesResetUri}" type="text/css" rel="stylesheet">
-                  <link href="${stylesUriMap.stylesMainUri}" type="text/css" rel="stylesheet">
+            <link href="${stylesUriMap["shivizStylesUri"]}" type="text/css" rel="stylesheet">
+                  <link href="${stylesUriMap["stylesResetUri"]}" type="text/css" rel="stylesheet">
+                  <link href="${stylesUriMap["stylesMainUri"]}" type="text/css" rel="stylesheet">
         </head>
         <body>
             <h1>
